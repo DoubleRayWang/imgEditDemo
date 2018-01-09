13fbe54796314cc3fcd2240c9d0f6bf506fa846d
doubleray@ctrchina.cn <wang269225>
2018-01-09 16:26:27 +0800

    AlloyClip优化

--- js/index.js
+++ js/index.js
@@ -5,11 +5,12 @@
 })*/
 /*
 $AC或AlloyClip 构造方法 初始化AlloyClip对象
-new $AC(String Selector, Number width, Number height, Number Style);
+new $AC(String Selector, Number width, Number height, Number Style, Array border-img-src);
 {Selector} DOM选择器
 {width} 要裁剪到宽度
 {height} 要裁剪的高度
 {Style} 使用的样式 0是不固定选择框，1是固定选择框
+{border-img-src} 边框样式图路径
 返回 AlloyClip对象
 * */
 /*
@@ -19,13 +20,16 @@ ok(Function callback)
 base64为裁剪好的图片base64字符串
 AIObj为裁剪好的图片AlloyImage对象
 * */
-new $AC("#_clip_", 800, 800, 0).ok(function (base64, aiObj) {
+new $AC("#_clip_", 800, 800, 0, [
+    "images/border/border1.png",
+    "images/border/border2.png"
+]).ok(function (base64, aiObj) {
     $('#_clip_').hide()
     var image = $('#imagedit');
-    image.append('<img src="'+ base64 +'">');
+    image.append('<img src="' + base64 + '">');
     image.children('img')[0].onload = function () {
         console.log(2);
-        if (this){
+        if (this) {
             $('img').imageEdit({
                 maxScale: 3,
                 onReady: function () {
